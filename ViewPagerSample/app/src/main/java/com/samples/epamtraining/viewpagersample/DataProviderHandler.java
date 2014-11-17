f281b7d2d83da738ed4429a7db67b521630daab5
green_2005 <green_2005@tut.by>
2014-11-17 11:47:02 +0300

    Delete old sample

--- ViewPagerSample/app/src/main/java/com/samples/epamtraining/viewpagersample/DataProviderHandler.java
+++
@@ -1,6 +0,0 @@
-package com.samples.epamtraining.viewpagersample;
-
-public interface DataProviderHandler {
-    public void onDataLoaded();
-    public void onException(Exception e);
-}
--- ViewPagerSample/app/src/main/java/com/samples/epamtraining/viewpagersample/Pages.java
+++
@@ -1,3 +0,0 @@
-package com.samples.epamtraining.viewpagersample;
-
-public enum Pages {NEWS, FRIENDS, MESSAGES, FAVORITIES, PROFILE };
