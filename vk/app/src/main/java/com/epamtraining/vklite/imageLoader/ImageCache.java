772bb63c58644008a760b5c8bd99b1ea6a97ae59
green <green_2005@tut.by>
2014-12-09 00:56:04 +0400

    ImageLoader concurrency

--- vk/app/src/main/java/com/epamtraining/vklite/imageLoader/ImageCache.java
+++ vk/app/src/main/java/com/epamtraining/vklite/imageLoader/ImageCache.java
@@ -5,18 +5,27 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.LruCache;
+
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class ImageCache {
     private Context mContext;
     private File filePath;
     private LruCache<String, Bitmap> mLruCache;
+    private Object mLruObject = new Object();
+    private Object mFileObject = new Object();
 
     public ImageCache(Context context) {
         mContext = context;
         if (android.os.Environment.getExternalStorageState().equals(
                 android.os.Environment.MEDIA_MOUNTED))
-            filePath = context.getExternalFilesDir(null);
+            filePath = context.getExternalFilesDir(null);  /*Returns absolute paths to application-specific directories on all
+                                                            * external storage devices where the application can place persistent files
+                                                            * it owns. These files are internal to the application, and not typically
+                                                            * visible to the user as media.*/
         else
             filePath = context.getCacheDir();
         if (!filePath.exists())
@@ -24,55 +33,118 @@ public class ImageCache {
 
         int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
         final int cacheSize = maxMemory / 8;
-
         mLruCache = new LruCache<String, Bitmap>(cacheSize) {
             @Override
             protected int sizeOf(String key, Bitmap bitmap) {
-                  return bitmap.getByteCount() / 1024;
+                return bitmap.getByteCount() / 1024;
             }
         };
     }
 
     public void addBitmapToLRUCache(String url, Bitmap bitmap) {
-        if (mLruCache.get(url) == null) {
-            mLruCache.put(url, bitmap);
+        synchronized (mLruObject) {
+            if (mLruCache.get(url) == null) {
+                mLruCache.put(url, bitmap);
+            }
         }
     }
 
-    private Bitmap getBitmapFromLRUCache(String url) {
-        return mLruCache.get(url);
+    public Bitmap getBitmapFromLRUCache(String url) {
+        synchronized (mLruObject) {
+            return mLruCache.get(url);
+        }
     }
 
     public String getFileName(String imageUrl) {
-        return filePath.getAbsolutePath() + File.separator + imageUrl.hashCode();
+        return filePath.getAbsolutePath() + File.separator + getMD5(imageUrl);
     }
 
-    public void removeImage(String url){
+    public void removeImage(String url) {
         String fileName = getFileName(url);
         File f = new File(fileName);
-        if (f.exists()) f.delete();
-        mLruCache.remove(url);
+        synchronized (mFileObject) {
+            if (f.exists()) f.delete();
+        }
+        synchronized (mLruObject) {
+            mLruCache.remove(url);
+        }
     }
 
     public Bitmap getImage(String url) {
-        Bitmap bmp = getBitmapFromLRUCache(url);
+        Bitmap bmp;
+        synchronized (mLruObject) {
+            bmp = getBitmapFromLRUCache(url);
+        }
         if (bmp == null) {
             String fileName = getFileName(url);
-            File f = new File(fileName);
-            if (f.exists()) {
-                BitmapFactory.Options options = new BitmapFactory.Options();
-                options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-                bmp = BitmapFactory.decodeFile(fileName, options);
-                addBitmapToLRUCache(url, bmp);
+            synchronized (mFileObject) {
+                File f = new File(fileName);
+                if (f.exists()) {
+                    BitmapFactory.Options options = new BitmapFactory.Options();
+                    options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+                    bmp = BitmapFactory.decodeFile(fileName, options);
+                    addBitmapToLRUCache(url, bmp);
+                }
             }
         }
         return bmp;
+    }
+
+    public void putImage(String imageUrl, InputStream is) throws Exception {
+        if (is == null) {
+            throw new Exception("InputStream is null");
+        }
+        FileOutputStream fOut = null;
+        try {
+            String mFileName = getFileName(imageUrl);
+            synchronized (mFileObject) {
+                File f = new File(mFileName);
+                f.createNewFile();
+                fOut = new FileOutputStream(f);
+                copyStreams(is, fOut);
+            }
+        } finally {
+            is.close();
+            if (fOut != null) {
+                fOut.flush();
+                fOut.close();
+            }
+        }
+    }
+
+    private void copyStreams(InputStream streamSource, OutputStream streamDest) throws Exception {
+        int BUFFER_SIZE = 1024;
+        byte[] buffer = new byte[BUFFER_SIZE];
+        try {
+            int cnt = 0;
+            while (cnt != -1) {
+                cnt = streamSource.read(buffer, 0, BUFFER_SIZE);
+                if (cnt != -1)
+                    streamDest.write(buffer, 0, cnt);
+            }
+        } catch (Exception e) {
+            throw new Exception(e);
+        }
     }
 
     public void clearImages() {
         for (File f : filePath.listFiles()) {
             f.delete();
         }
+    }
+
+    private String getMD5(String md5) {
+        try {
+            java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+            byte[] array = md.digest(md5.getBytes());
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < array.length; ++i) {
+                sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1, 3));
+            }
+            return sb.toString();
+        } catch (java.security.NoSuchAlgorithmException e) {
+        }
+        return null;
     }
 
     public String getFilecahedir() {
--- vk/app/src/main/java/com/epamtraining/vklite/imageLoader/ImageLoader.java
+++ vk/app/src/main/java/com/epamtraining/vklite/imageLoader/ImageLoader.java
@@ -6,81 +6,85 @@ import android.widget.ImageView;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.os.Build;
 import android.os.Handler;
 import android.text.TextUtils;
 
 import com.epamtraining.vklite.os.VKExecutor;
 
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.URL;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 
 public class ImageLoader {
-    private class LoadingImages{
-        private Map<Integer, HashSet<ImageView>> loadingImagesList;
-        LoadingImages(){
-            loadingImagesList = new ConcurrentHashMap<Integer,HashSet<ImageView>>();
+    private class LoadingImages {
+        private Map<String, CopyOnWriteArraySet<ImageView>> mLoadingImagesList;
+        private Map<String, Runnable> mRunningThreads;
+
+        LoadingImages() {
+            mLoadingImagesList = new ConcurrentHashMap<String, CopyOnWriteArraySet<ImageView>>();
+            mRunningThreads = new ConcurrentHashMap<String, Runnable>();
         }
 
-        void addImage(Integer hashCode, ImageView imageView){
-            HashSet<ImageView> imageViews = loadingImagesList.get(hashCode);
-            if (imageViews == null){
-                imageViews = new HashSet<ImageView>();
+        void addImage(String url, ImageView imageView) {
+            CopyOnWriteArraySet<ImageView> imageViews = mLoadingImagesList.get(url);
+            if (imageViews == null) {
+                imageViews = new CopyOnWriteArraySet<ImageView>();
                 imageViews.add(imageView);
-                loadingImagesList.put(hashCode, imageViews);
-            } else
-            {
+                mLoadingImagesList.put(url, imageViews);
+            } else {
                 imageViews.add(imageView);
             }
         }
 
-        void loadingDone(Integer hashCode){
-            loadingImagesList.remove(hashCode);
+        void addThread(String url, Runnable imageThread) {
+            mRunningThreads.put(url, imageThread);
         }
 
-        boolean isImageLoading(Integer hashCode){
-            return loadingImagesList.containsKey(hashCode);
+        Set<Runnable> getThreads() {
+            HashSet<Runnable> threads = new HashSet<Runnable>();
+            for (String s : mRunningThreads.keySet()) {
+                threads.add(mRunningThreads.get(s));
+            }
+            return threads;
         }
 
-        HashSet<ImageView> getImageViews(Integer hashCode){
-            return loadingImagesList.get(hashCode);
+        void loadingDone(String url) {
+            mLoadingImagesList.remove(url);
+            mRunningThreads.remove(url);
+        }
+
+        boolean isImageLoading(String url) {
+            return mLoadingImagesList.containsKey(url);
+        }
+
+        private void clear() {
+            mRunningThreads.clear();
+            mLoadingImagesList.clear();
+        }
+
+        Set<ImageView> getImageViews(String url) {
+            return mLoadingImagesList.get(url);
         }
     }
-
-    private enum ImageThreadState{
-      THREAD_DELETED,
-      THREAD_NOT_EXISTS,
-      THREAD_NOT_DELETED
-    };
-
-    private static int LOAD_IMAGE_DELAY = 400;
+    
+    //private static int LOAD_IMAGE_DELAY = 400;
     private ImageCache mCache;
-
-    private Context mContext;
-    private long imagesLoaded = 0;
     private Handler mHandler;
     private LoadingImages mLoadingList;
-    //private Map<Integer, Runnable> mLoadingList;
     private VKExecutor mExecutor;
-
 
     public ImageLoader(Context context) {
         mHandler = new Handler();
-        mContext = context;
         mCache = new ImageCache(context);
         mExecutor = new VKExecutor(VKExecutor.ExecutorServiceType.BITMAP);
-       // if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) needBitmapRecycle = true;
-        mLoadingList = new LoadingImages();//ConcurrentHashMap<Integer, Runnable>();
+        // if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) needBitmapRecycle = true;
+        mLoadingList = new LoadingImages();
     }
 
     public void loadImage(final ImageView imageView, final String url) {
@@ -90,7 +94,12 @@ public class ImageLoader {
         imageView.setImageBitmap(null);
         imageView.setTag(url);
         if (!TextUtils.isEmpty(url)) {
-           new LoadThread(mHandler, imageView, url ).start();
+            Bitmap bmp = mCache.getBitmapFromLRUCache(url);
+            if (bmp != null) {
+                imageView.setImageBitmap(bmp);
+            } else {
+                new LoadThread(mHandler, imageView, url).start();
+            }
         }
     }
 
@@ -98,13 +107,11 @@ public class ImageLoader {
         Handler mHandler;
         ImageView mImageView;
         String mUrl;
-        String mFileName;
 
-        LoadThread(Handler handler, ImageView imageView, String url ) {
+        LoadThread(Handler handler, ImageView imageView, String url) {
             mHandler = handler;
             mImageView = imageView;
             mUrl = url;
-            mFileName = mCache.getFileName(url);
         }
 
         public void run() {
@@ -119,101 +126,65 @@ public class ImageLoader {
                         }
                     });
                 } else {
-                    final int imageHashCode = mUrl.hashCode();
-                    if (mLoadingList.isImageLoading(imageHashCode)){
-                        mLoadingList.addImage(imageHashCode, mImageView);
+                    if (mLoadingList.isImageLoading(mUrl)) {
+                        mLoadingList.addImage(mUrl, mImageView);
                         Log.d("image is loading", "image");
                     } else {
 //                    Thread.sleep(LOAD_IMAGE_DELAY);
-                        mLoadingList.addImage(imageHashCode, mImageView);
-                        doLoadImage(mImageView, mUrl, mFileName, mHandler, imageHashCode);
+                        mLoadingList.addImage(mUrl, mImageView);
+                        doLoadImage(mImageView, mUrl, mHandler);
                     }
                 }
             } catch (Exception e) {
-                e.printStackTrace();
-            };
+                mCache.removeImage(mUrl);
+                mLoadingList.loadingDone(mUrl);
+            }
+            ;
         }
     }
 
-    private void doLoadImage(final ImageView imageView, final String imageUrl, final String mFileName, final Handler mHandler,
-                             final int imageHashCode) {
+    private void doLoadImage(final ImageView imageView, final String imageUrl, final Handler mHandler) {
         Runnable imageThread = new Runnable() {
             @Override
             public void run() {
+                InputStream is = null;
                 try {
-                    File f = new File(mFileName);
-                    f.createNewFile();
-                    InputStream is = null;
-                    FileOutputStream fOut = null;
                     URL url = new URL(imageUrl);
-                    try {
-                        is = url.openStream();
-                        fOut = new FileOutputStream(f);
-                        copyStreams(is, fOut);
-                    } finally {
-                       is.close();
-                        if (fOut != null) {
-                            fOut.flush();
-                            fOut.close();
-                        }
-                    }
-                    FileInputStream bitmapStream = new FileInputStream(mFileName);
-                    try {
-                        final Bitmap bmp = BitmapFactory.decodeStream(bitmapStream);
-                        mCache.addBitmapToLRUCache(imageUrl, bmp);
-                        mHandler.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                    for (ImageView image:mLoadingList.getImageViews(imageHashCode)){
-                                        if (imageUrl.equals(image.getTag()))
-                                            image.setImageBitmap(bmp);
-                                    }
-                                mLoadingList.loadingDone(imageHashCode);
+                    is = url.openStream();
+                    mCache.putImage(imageUrl, is);
+                    final Bitmap bmp = mCache.getImage(imageUrl);
+                    mCache.addBitmapToLRUCache(imageUrl, bmp);
+                    mHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Set<ImageView> imageViews = mLoadingList.getImageViews(imageUrl);
+                            if (imageViews != null) {
+                                for (ImageView image : imageViews) {
+                                    if (imageUrl.equals(image.getTag()))
+                                        image.setImageBitmap(bmp);
+                                }
+                                mLoadingList.loadingDone(imageUrl);
                             }
-                        });
-                    } finally {
-                        bitmapStream.close();
-                    }
+                        }
+                    });
                 } catch (Exception e) {
                     mCache.removeImage(imageUrl);
+                    mLoadingList.loadingDone(imageUrl);
                 }
             }
         };
-       // addLoadingImage(imageHashCode, imageThread);
+        mLoadingList.addThread(imageUrl, imageThread);
         mExecutor.start(imageThread);
-//        new VKExecutor(VKExecutor.ExecutorServiceType.BITMAP, imageThread).start();
     }
 
-    private void copyStreams(InputStream streamSource, OutputStream streamDest) throws Exception {
-        int BUFFER_SIZE = 1024;
-        byte[] buffer = new byte[BUFFER_SIZE];
-        try {
-            int cnt = 0;
-            while (cnt != -1) {
-                cnt = streamSource.read(buffer, 0, BUFFER_SIZE);
-                if (cnt != -1)
-                    streamDest.write(buffer, 0, cnt);
-            }
-        } catch (Exception e) {
-            throw new Exception(e);
+    public void stopLoadingImages() {
+        for (Runnable r : mLoadingList.getThreads()) {
+            mExecutor.remove(r);
         }
-    }
-
-    public String getImageCacheDir() {
-        return mCache.getFilecahedir();
-    }
-
-    public void stopLoadingImages(){
-//        synchronized (mLoadingList){
-//            for (int hashCode  : mLoadingList.keySet()){
-//                mExecutor.remove(mLoadingList.get(hashCode));
-//            }
-//            mLoadingList.clear();
-//        }
+        mLoadingList.clear();
     }
 
     public void clear() {
         mCache.clearImages();
     }
-
 }
